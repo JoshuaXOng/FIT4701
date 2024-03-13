82949f62e013deba065927c6de4846c086a3eb0c
Joshua Ong <joshuaong2000@gmail.com>
2024-03-14 01:53:00 +1100

    Misc.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import os
 import logging
 import datetime
+import itertools
+import argparse
 import readline
 import csv
 import pandas as pd
@@ -9,7 +11,7 @@ import matplotlib
 import matplotlib.pyplot as plt, mpld3
 import numpy as np 
 import h5py
-import argparse
+from sklearn.linear_model import LinearRegression
 
 RADAR_RANGE_MATPLOTLIB_IMSHOW = {
     'vmin': 0,
@@ -72,18 +74,26 @@ def main():
     sensors_dataset = radar_file['data']
     def reoriente_sensor_data(sensors_data):
         return sensors_data.swapaxes(0, 1)[:][0].swapaxes(0, 1)
+    def get_radar_file_start_timestamp(radar_file):
+        return datetime.datetime.fromtimestamp(
+            min(radar_file['sample_times'][:])
+        ) 
+    def get_radar_file_end_timestamp(radar_file):
+        return datetime.datetime.fromtimestamp(
+            max(radar_file['sample_times'][:])
+        ) 
     def get_radar_data_around_timestamp(radar_file, center_datetime, leniency_timedelta):
         if radar_file['sample_times'].shape[0] != radar_file['data'].shape[0]:
             logging.warning('Data and timestamps are not of the same length.')
         
-        closest_data, smallest_timedelta = None, None
-        for data_index, data_timestamp in enumerate(radar_file['sample_times'][:]):
-            candidate_timedelta = center_datetime - datetime.datetime.fromtimestamp(data_timestamp)
-            if abs(candidate_timedelta.total_seconds()) < leniency_timedelta.total_seconds():
-                if (closest_data is None and smallest_timedelta is None) \
-                or (abs(candidate_timedelta.total_seconds()) < smallest_timedelta.total_seconds()):
-                    closest_data, smallest_timedelta = radar_file['data'][data_index], candidate_timedelta
-        return closest_data
+        closest_data, smallest_timedelta = min(
+            zip(radar_file['data'][:], radar_file['sample_times'][:]), 
+            key=lambda x: abs((center_datetime - datetime.datetime.fromtimestamp(x[1])).total_seconds())
+        )
+        if abs((center_datetime - datetime.datetime.fromtimestamp(smallest_timedelta)).total_seconds()) \
+        >= leniency_timedelta.total_seconds():
+            return None
+        return closest_data     
 
     logging.info('Start timestamp in radar file is... {}'.format(start_timestamp))
     logging.info('Shape of data in radar file is... {}'.format(sensors_dataset.shape))
@@ -92,6 +102,10 @@ def main():
         environment_information = pd.read_excel(program_arguments.environment_file).iloc[1:, :]
         def environment_timestamp_to_datetime(environment_timestamp):
             return datetime.datetime.strptime(environment_timestamp, "%m/%d/%y %H:%M")
+        def get_environment_information_start_timestamp(environment_information):
+            return min(environment_information.iloc[:, 0])
+        def get_environment_information_end_timestamp(environment_information):
+            return max(environment_information.iloc[:, 0])
         def get_environment_information_around_timestamp(environment_information, center_datetime, leniency_timedelta):
             closest_row, smallest_timedelta = None, None
             for row_information in environment_information.iterrows():      
@@ -106,18 +120,50 @@ def main():
         logging.info('Shape of the environment file... {}'.format(environment_information.shape))
         logging.info('Initial slice of environment file... {}'.format(environment_information.iloc[:2, :]))
 
-        from sklearn.ensemble import RandomForestRegressor
-        from sklearn.linear_model import LinearRegression
-        model = RandomForestRegressor()
-        model = LinearRegression()
+        radar_start, radar_end = get_radar_file_start_timestamp(radar_file), get_radar_file_end_timestamp(radar_file)
+        environment_start, environment_end = get_environment_information_start_timestamp(environment_information), \
+        get_environment_information_end_timestamp(environment_information)
+        
+        logging.info('Radar data starts at... {}, and ends at... {}'.format(radar_start, radar_end)) 
+        logging.info('Environment information starts at... {}, and ends at... {}'.format(environment_start, environment_end))
+        if radar_start is None  or radar_end is None or environment_start is None or environment_end is None:
+            raise Exception('Radar data interval ({} to {}) or environment data ({} to {}) interval based on'
+            ' supplied data is in-complete.'.format(radar_start, radar_end, environment_start, environment_end))
+        
+        latest_start = max(radar_start, environment_start)
+        earliest_end = min(radar_end, environment_end)
+        
+        radar_and_moisture = []
+        for row_index, _environment_information in enumerate(environment_information.iterrows()):   
+            logging.debug('At index {} of {}'.format(row_index, environment_information.shape[0]))
+
+            environment_timestamp = _environment_information[1]['TIMESTAMP'] 
+            
+            if environment_timestamp < latest_start or environment_timestamp > earliest_end:
+                logging.debug('Skipping finding corresponding radar data, latest start is... {}, earliest end is... {} and '
+                'environment timestamp is... {}'.format(latest_start, earliest_end, environment_timestamp))
+                continue
+            
+            corresponding_radar = get_radar_data_around_timestamp(radar_file, environment_timestamp, datetime.timedelta(seconds=1))
+            
+            if corresponding_radar is None:
+                logging.warning('No corresponding radar data found for environment data of timestamp... {}'.format(environment_timestamp)) 
+                continue 
+            
+            radar_and_moisture.append((_environment_information, corresponding_radar))
+
+        print(len(radar_and_moisture))
+            
+        moisture_model = LinearRegression()
         x = []
         y = []
         for i in range(0, 1000):
             x.append([i])
             y.append(i) 
         print(x[:4], y[:4])
-        model.fit(x, y)    
-        print(model.predict([[6]]))
+        moisture_model.fit(x, y)    
+        print(moisture_model.predict([[6]]))
+        return 
 
     subplots_figure, subplots_ax = plt.subplots()
     subplots_ax.set_title('Radar Heatmap')
