1fd5071a381173ff5d33b17daa4ebb123c01ff21
Joshua Ong <joshuaong2000@gmail.com>
2024-07-27 00:41:58 +1000

    SPI attempt

--- stm_sandbox/src/main.rs
+++ stm_sandbox/src/main.rs
@@ -1,99 +1,66 @@
-#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-use core::{fmt::Write, str};
+use panic_halt as _;
+
+use stm32f4xx_hal::{gpio::Speed, pac, prelude::*, spi::{Mode, Phase, Polarity, Spi}};
 use cortex_m_rt::entry;
-use nucleo_f446re::{led::LedDigital, serial::SerialPort, Nucleo};
-use panic_probe as _;
-use stm32f4xx_hal::{block, prelude::*};
 use rtt_target::{rtt_init_print, rprintln};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Can now print"); 
 
-    let mut nucleo = Nucleo::<LedDigital>::init().unwrap();
-    let SerialPort { mut tx, mut rx } = nucleo.vcom;
+    let board_peripherals = pac::Peripherals::take().unwrap();
+    let gpioa = board_peripherals.GPIOA.split();
+    let gpiob = board_peripherals.GPIOB.split();
 
-    let greeting = [
-        "Welcome to the Nucleo command center terminal!",
-        "You can:",
-        "\tblinky on",
-        "\tblinky off",
-    ];
-    tx.write_str("\r\n").unwrap();
-    for s in greeting {
-        tx.write_str(s).unwrap();
-        tx.write_str("\r\n").unwrap();
-    }
+    // let mut pa9 = gpioa.pa0.into_push_pull_output(); // PA9, a.k.a, D8. Interrupt line. 
 
-    let mut line = [0_u8; 20];
-    let mut line_idx = 0;
-    let mut clear = true;
+    let mut pb0 = gpiob.pb0.into_push_pull_output(); // A3, a.k.a, PB0. Sensor enablement (active high). TODO: ADC?
+    pb0.set_high(); 
+    rprintln!("pb0 is {:?}", pb0.get_state());
+
+    let mut pb6 = gpiob.pb6.into_push_pull_output(); // PB6, a.k.a, D10. Slave select.
+    pb6.set_low();
+    rprintln!("pb6 is {:?}", pb6.get_state());
+
+    let mut xe121_spi = Spi::new(
+        board_peripherals.SPI1, // XE121 connects to SPI1 of Nucleo board.
+        (
+            gpioa.pa5.into_alternate().speed(Speed::VeryHigh), // PA5, a.k.a, D13.
+            gpioa.pa6.into_alternate().speed(Speed::VeryHigh) // PA6, a.k.a, D12.
+                .internal_pull_up(true), // TODO: When do you want to enable this?
+            gpioa.pa7.into_alternate().speed(Speed::VeryHigh) // PA7, a.k.a., D11.
+                .internal_pull_up(true) // TODO: When do you want to enable this?
+        ), 
+        Mode {
+            polarity: Polarity::IdleHigh, // TODO: IdleHigh is presumably active low, correct?
+            phase: Phase::CaptureOnSecondTransition, // "MISO changes value on the falling edge".
+                                                    
+        }, 
+        3.MHz(), // Max is 50 MHz
+        &board_peripherals.RCC.constrain().cfgr.freeze()
+    );
+    xe121_spi.enable(true);
+
+    let gpioc = board_peripherals.GPIOC.split();
+    let mut led = gpioc.pc13.into_push_pull_output();
+
     loop {
-        if clear {
-            clear = false;
-            line = [0_u8; 20];
-            line_idx = 0;
-            tx.write_str("\r\n? ").unwrap();
+        let mut a121_readings = [12; 4];
+        rprintln!("Ahh... {:?}", a121_readings); 
+        if let Err(e) = xe121_spi.read(&mut a121_readings) {
+            rprintln!("Failed to get sensor readings... {:?}", e); 
+        } else {
+            rprintln!("Sensor readings... {:?}", a121_readings); 
         }
 
-        if let Ok(c) = block!(rx.read()) {
-            let _ = block!(tx.write(c));
-
-            if line_idx < line.len() {
-                if c == b'\r' || c == b'\n' {
-                    // Process command after a line ending is received
-
-                    // Rust str's don't play nice with null terminations so subslice to remove it
-                    let null_pos = line.iter().position(|&c| c == b'\0').unwrap_or(line.len()); // Default to line length if no null present
-                    let cmd = &line[0..null_pos];
-
-                    match str::from_utf8(cmd) {
-                        Ok(s) => {
-                            match s {
-                                "blinky on" => {
-                                    nucleo.user_led.on();
-                                    tx.write_str("\r\nYou light up the room.").unwrap();
-                                }
-                                "blinky off" => {
-                                    nucleo.user_led.off();
-                                    tx
-                                        .write_str("\r\nIt is pitch black. You are likely to be eaten by a grue.")
-                                        .unwrap();
-                                }
-                                "" => (), // If sending two line endings in a row for example
-                                _ => {
-                                    write!(tx, "\r\nOops, invalid command: {:?}", s).unwrap();
-                                }
-                            }
-                        }
-                        Err(_) => {
-                            write!(tx, "Oops, invalid str: {:?}", line).unwrap();
-                        }
-                    }
-                    clear = true;
-                } else if c == b'\x08' || c == b'\x7F' {
-                    // Backspace
-                    if line_idx > 0 {
-                        line_idx -= 1;
-                    }
-                    line[line_idx] = 0;
-                } else {
-                    // Anything else
-                    line[line_idx] = c;
-                    line_idx += 1;
-                }
-            } else {
-                tx.write_str("\r\nOops, line too long.").unwrap();
-                clear = true;
-            }
-        } else {
-            tx.write_str("\r\nOops, something went wrong reading a character.")
-                .unwrap();
-            clear = true;
+        for _ in 0..10_000 {
+            led.set_high();
+        }
+        for _ in 0..10_000 {
+            led.set_low();
         }
     }
 }
