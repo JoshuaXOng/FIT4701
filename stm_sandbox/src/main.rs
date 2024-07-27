81c81d27ed800a9506603507d450a829c4d1ff2d
Joshua Ong <joshuaong2000@gmail.com>
2024-07-27 11:02:45 +1000

    Readings...

--- stm_sandbox/src/main.rs
+++ stm_sandbox/src/main.rs
@@ -3,7 +3,7 @@
 
 use panic_halt as _;
 
-use stm32f4xx_hal::{gpio::Speed, pac, prelude::*, spi::{Mode, Phase, Polarity, Spi}};
+use stm32f4xx_hal::{gpio::Speed, pac, prelude::*, spi::{Mode, Phase, Polarity, Spi}, timer::Timer};
 use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 
@@ -12,6 +12,19 @@ fn main() -> ! {
     rtt_init_print!();
 
     let board_peripherals = pac::Peripherals::take().unwrap();
+    let chip_peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
+
+    let clock_control = board_peripherals.RCC.constrain();
+    let system_clocks = clock_control
+        .cfgr
+        .sysclk(180.MHz())
+        .hclk(180.MHz())
+        .pclk1(30.MHz())
+        .pclk2(30.MHz())
+        .freeze();
+
+    let mut delay = Timer::syst(chip_peripherals.SYST, &system_clocks).delay();
+
     let gpioa = board_peripherals.GPIOA.split();
     let gpiob = board_peripherals.GPIOB.split();
 
@@ -21,46 +34,37 @@ fn main() -> ! {
     pb0.set_high(); 
     rprintln!("pb0 is {:?}", pb0.get_state());
 
-    let mut pb6 = gpiob.pb6.into_push_pull_output(); // PB6, a.k.a, D10. Slave select.
-    pb6.set_low();
-    rprintln!("pb6 is {:?}", pb6.get_state());
-
     let mut xe121_spi = Spi::new(
         board_peripherals.SPI1, // XE121 connects to SPI1 of Nucleo board.
         (
             gpioa.pa5.into_alternate().speed(Speed::VeryHigh), // PA5, a.k.a, D13.
-            gpioa.pa6.into_alternate().speed(Speed::VeryHigh) // PA6, a.k.a, D12.
+            gpioa.pa6.into_alternate() // PA6, a.k.a, D12.
                 .internal_pull_up(true), // TODO: When do you want to enable this?
-            gpioa.pa7.into_alternate().speed(Speed::VeryHigh) // PA7, a.k.a., D11.
+            gpioa.pa7.into_alternate() // PA7, a.k.a., D11.
                 .internal_pull_up(true) // TODO: When do you want to enable this?
         ), 
-        Mode {
-            polarity: Polarity::IdleHigh, // TODO: IdleHigh is presumably active low, correct?
-            phase: Phase::CaptureOnSecondTransition, // "MISO changes value on the falling edge".
-                                                    
+        Mode { // CPOL/CPHA = 0
+            polarity: Polarity::IdleLow, 
+            phase: Phase::CaptureOnFirstTransition, 
         }, 
         3.MHz(), // Max is 50 MHz
-        &board_peripherals.RCC.constrain().cfgr.freeze()
+        &system_clocks
     );
     xe121_spi.enable(true);
 
-    let gpioc = board_peripherals.GPIOC.split();
-    let mut led = gpioc.pc13.into_push_pull_output();
+    let mut slave_select = gpiob.pb6.into_push_pull_output(); // PB6, a.k.a, D10. Slave select.
 
     loop {
-        let mut a121_readings = [12; 4];
-        rprintln!("Ahh... {:?}", a121_readings); 
-        if let Err(e) = xe121_spi.read(&mut a121_readings) {
+        slave_select.set_low();
+        delay.delay_ns(2);
+
+        let mut a121_readings = [0xaa; 16];
+        if let Err(e) = xe121_spi.transfer(&mut a121_readings, &[0xd0; 16]) {
             rprintln!("Failed to get sensor readings... {:?}", e); 
         } else {
             rprintln!("Sensor readings... {:?}", a121_readings); 
         }
 
-        for _ in 0..10_000 {
-            led.set_high();
-        }
-        for _ in 0..10_000 {
-            led.set_low();
-        }
+        slave_select.set_high();
     }
 }
